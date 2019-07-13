652dc0134cbfffabafa7de89b3e9f61d84e2c4aa
dsrtf0x <47214918+dsrtf0x@users.noreply.github.com>
2019-07-13 13:01:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict';
 
 $(document).ready(() => {
@@ -9,4 +10,30 @@ $(document).ready(() => {
 	$('#signupButton').click(() => {
 		$('#signupSection').slideDown(600);
 	});	
+
+	$(function() {
+  		$('.js-description_readmore').moreLines({
+    		linecount: 5,
+    		baseclass: 'b-description',
+    		basejsclass: 'js-description',
+    		classspecific: '_readmore',
+    		buttontxtmore: "Read more",
+    		buttontxtless: "Рома, спизжено",
+    		animationspeed: 250
+  		});
+	});
+
+=======
+'use strict';
+
+$(document).ready(() => {
+
+	$('#loginButton').click(() => {
+		$('#loginSection').slideDown(600);
+	});
+
+	$('#signupButton').click(() => {
+		$('#signupSection').slideDown(600);
+	});	
+>>>>>>> a248f8b80d6781f019d02a06a5b58d586d7d0e4a
 });
