ec4fe2c52b396607a960be0c4246c97662e1435c
dsrtf0x <47214918+dsrtf0x@users.noreply.github.com>
2019-07-13 13:07:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-'use strict';
-
 $(document).ready(() => {
 
 	$('#loginButton').click(() => {
@@ -22,18 +19,4 @@ $(document).ready(() => {
     		animationspeed: 250
   		});
 	});
-
-=======
-'use strict';
-
-$(document).ready(() => {
-
-	$('#loginButton').click(() => {
-		$('#loginSection').slideDown(600);
-	});
-
-	$('#signupButton').click(() => {
-		$('#signupSection').slideDown(600);
-	});	
->>>>>>> a248f8b80d6781f019d02a06a5b58d586d7d0e4a
 });
